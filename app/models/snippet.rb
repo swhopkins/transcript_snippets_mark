e13caee6cdc196fa72656f6f386802a36b6b9526
Mark Lilien <mark@textline.com>
2025-08-19 13:32:43 -0700

    feat: adding validations and relations to Snippet

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,8 @@
 class Snippet < ApplicationRecord
-  # TODO: Add association to transcript
-  # TODO: Add validations for required fields
+  belongs_to :transcript
+
+  validates :start, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :end,   presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :text,  presence: true
   # TODO: Add search scope for text searching
 end
